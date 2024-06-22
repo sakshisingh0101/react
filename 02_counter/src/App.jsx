aeca723fad7ab2d794f71a8ace58abb9a4eea502
sakshisingh0101 <165368380+sakshisingh0101@users.noreply.github.com>
2024-06-22 20:08:47 +0000

    bgchanger

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -10,8 +10,20 @@ function App() {
   const addcount=()=>{
     if(counter+1<=20)
       {
-    counter=counter+1;
-    setcounter(counter)
+    // counter=counter+1;
+    //value ka increment ek hi baar hoga agr 15 h to 19 nhi ho jayegi kyunki ek hi state se call lag rhi h baar baar same call lag rhi h 
+    //bunch of packages call ja rhi h like ek hi to kaam h wahi repeat ho rha h 
+    setcounter(counter+1)
+    setcounter(counter+1)
+    setcounter(counter+1)
+    setcounter(counter+1)
+    //same cheez agr call back function leke kare to ek call complete honeke baad dusri lagti h i mean pura update hone ke baad
+    //bunch of packages ki tarah call nhi jaa rhi
+    // setcounter((prevcounter)=>prevcounter+1);
+    // setcounter((prevcounter)=>prevcounter+1);
+    // setcounter((prevcounter)=>prevcounter+1);
+    // setcounter((prevcounter)=>prevcounter+1);
+
       }
     // console.log("val updated")
   }
---
+++ 04_backgroundchanger/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  // const body=document.querySelector('body')
+  // body.style.backgroundColor='black';
+  const [color,setColor]=useState('black')
+ 
+
+  return (
+    <>
+      <div className='w-full h-screen ' style={{backgroundColor:color}}>
+      <button className='bg-red-400 text-black rounded-xl ml-5' onClick={()=> setColor('red')} > Red </button>
+      <button  className='bg-lavendar-400 text-black rounded-xl ml-5' onClick={()=> setColor('lavender')} > lavender </button>
+      <button  className='bg-green-400 text-black rounded-xl ml-5' onClick={()=> setColor('green')} > green </button>
+      <button  className='bg-purple-400 text-black rounded-xl ml-5' onClick={()=> setColor('purple')} > purple</button>
+      <button  className='bg-yellow-400 text-black rounded-xl ml-5' onClick={()=> setColor('yellow')} > yellow </button>
+      <button  className='bg-black-400 text-black rounded-xl ml-5' onClick={()=> setColor('black')} > black </button>
+      <button  className='bg-white-400 text-black rounded-xl ml-5' onClick={()=> setColor('white')} > white </button>
+      <button  className='bg-orange-400 text-black rounded-xl ml-5' onClick={()=> setColor('orange')} > orange </button>
+      <button  className='bg-pink-400 text-black rounded-xl ml-5' onClick={()=> setColor('pink')} > pink </button>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
