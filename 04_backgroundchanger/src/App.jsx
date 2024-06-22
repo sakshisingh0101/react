b76eaa2e6a2a574a6542fc0f466dd99a59012881
sakshisingh0101 <165368380+sakshisingh0101@users.noreply.github.com>
2024-06-22 20:36:18 +0000

    bgchanger update

--- 04_backgroundchanger/src/App.jsx
+++ 04_backgroundchanger/src/App.jsx
@@ -7,13 +7,13 @@ function App() {
   const [count, setCount] = useState(0)
   // const body=document.querySelector('body')
   // body.style.backgroundColor='black';
-  const [color,setColor]=useState('black')
+  const [color,setColor]=useState('pink')
  
 
   return (
     <>
-      <div className='w-full h-screen ' style={{backgroundColor:color}}>
-      <button className='bg-red-400 text-black rounded-xl ml-5' onClick={()=> setColor('red')} > Red </button>
+      <div className='w-full h-screen fixed flex flex-wrap justify-center bottom-1 inset-x-0 px-2 items-center content-end ' style={{backgroundColor:color}}>
+      <button className='bg-red-400 text-black rounded-xl ml-5 h-10' onClick={()=> setColor('red')} > Red </button>
       <button  className='bg-lavendar-400 text-black rounded-xl ml-5' onClick={()=> setColor('lavender')} > lavender </button>
       <button  className='bg-green-400 text-black rounded-xl ml-5' onClick={()=> setColor('green')} > green </button>
       <button  className='bg-purple-400 text-black rounded-xl ml-5' onClick={()=> setColor('purple')} > purple</button>
